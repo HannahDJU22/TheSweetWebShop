703c616946b457a00998115382e6e42494b0b6a7
hannah <hannah.degerheim@iths.se>
2023-04-20 08:54:37 +0200

    Initial commit

---
+++ src/test/java/se/iths/thesweetwebshop/TheSweetWebShopApplicationTests.java
@@ -0,0 +1,13 @@
+package se.iths.thesweetwebshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TheSweetWebShopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
